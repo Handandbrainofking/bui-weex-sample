7076043c795f10188e0accd25486d23dde6f8188
tonyyls <yulsh@bingosoft.net>
2017-08-01 15:20:35 +0800

    增加placeholder示例

--- src/views/example/image-demo.vue
+++ src/views/example/image-demo.vue
@@ -8,13 +8,16 @@
 
 
         <scroller class="span1" style="padding: 10px">
-            <div style="height: 2000px">
+            <div style="height: 2500px">
                 <text class="h4">加载远程图片</text>
                 <bui-image width="260px" height="260px" @load="onLoad()"
                            src="https://img.alicdn.com/tps/TB1z.55OFXXXXcLXXXXXXXXXXXX-560-560.jpg"></bui-image>
 
                 <text class="h4">加载工程目录的图片(固定放在image目录下)</text>
                 <bui-image src="/image/demo.png" width="300px" height="300px"></bui-image>
+                <text class="h4">使用placeholder</text>
+                <bui-image width="260px" placeholder="/image/demo.png" height="260px" @load="onLoad()"
+                           src="https://img.alicdn.1com/tps/TB1z.55OFXXXXcLXXXXXXXXXXXX-560-5601.jpg"></bui-image>
                 <text class="h4">圆角图片(radius="30px")</text>
                 <bui-image width="560px" height="560px"
                            radius="30px"
@@ -28,6 +31,7 @@
                         <text style="font-size:50px; color: #ff0000">Hello，Bui-Weex.</text>
                     </div>
                 </div>
+
             </div>
         </scroller>
     </div>
